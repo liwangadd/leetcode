32fab61803220c014d1e7ee8905e3c6fe5a3a37a
liwangadd <liwangadd@gmail.com>
2018-09-17 09:08:29 +0800

    T122&T136&T141&T155...

---
+++ src/main/java/T136.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class T136 {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            res ^= num;
+        }
+        return res;
+    }
+}
---
+++ src/main/java/T169.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class T169 {
+
+    public int majorityElement(int[] nums) {
+        if (nums.length <= 2) return nums[0];
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+
+}

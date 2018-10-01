74c324c7d5b5d780b4290a5de4634dcc6e163042
liwangadd <liwangadd@gmail.com>
2018-10-01 11:13:49 +0800

    T144&T145&T150&T71&T94

---
+++ src/main/java/T150.java
@@ -0,0 +1,45 @@
+import java.util.Stack;
+
+public class T150 {
+
+    public static int evalRPN(String[] tokens) {
+        if (tokens == null || tokens.length == 0) return 0;
+        Stack<Integer> numStack = new Stack<>();
+        Integer n1, n2;
+        for (String token : tokens) {
+            switch (token) {
+                case "+":
+                case "-":
+                    n2 = numStack.pop();
+                    n1 = numStack.pop();
+                    if (token.equals("+")) {
+                        numStack.push(n1 + n2);
+                    } else {
+                        numStack.push(n1 - n2);
+                    }
+                    break;
+                case "*":
+                case "/":
+                    n2 = numStack.pop();
+                    n1 = numStack.pop();
+                    if (token.equals("*")) {
+                        numStack.push(n1 * n2);
+                    } else {
+                        numStack.push(n1 / n2);
+                    }
+                    break;
+                default:
+                    numStack.push(Integer.valueOf(token));
+                    break;
+            }
+        }
+        return numStack.pop();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(evalRPN(new String[]{"2", "1", "+", "3", "*"}));
+        System.out.println(evalRPN(new String[]{"4", "13", "5", "/", "+"}));
+        System.out.println(evalRPN(new String[]{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"}));
+    }
+
+}

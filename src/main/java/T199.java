041138c91fb57d7da9e749ccd3aecb24857185f3
liwangadd <liwangadd@gmail.com>
2018-10-02 10:55:13 +0800

    T102&T103&T199&T341

---
+++ src/main/java/T199.java
@@ -0,0 +1,42 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class T199 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new LinkedList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int count = 1;
+        while (!queue.isEmpty()) {
+            int currentCount = count;
+            count = 0;
+            while (currentCount-- > 0) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.add(node.left);
+                    ++count;
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                    ++count;
+                }
+                if (currentCount == 0) res.add(node.val);
+            }
+        }
+        return res;
+    }
+
+}

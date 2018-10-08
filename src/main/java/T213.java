b6b7f1a122b32e2490cf341a0950fcd05c84eb9c
liwangadd <liwangadd@gmail.com>
2018-10-08 09:20:46 +0800

    T120&T213&T343

---
+++ src/main/java/T213.java
@@ -0,0 +1,23 @@
+public class T213 {
+
+    public static int rob(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        return Math.max(robSub(nums, 0, nums.length - 1), robSub(nums, 1, nums.length));
+    }
+
+    private static int robSub(int[] nums, int start, int end) {
+        int[] dp = new int[nums.length + 1];
+        dp[start] = 0;
+        dp[start + 1] = nums[start];
+        for (int i = start + 2; i <= end; ++i) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+        }
+        return dp[end];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(rob(new int[]{2, 3}));
+        System.out.println(rob(new int[]{1, 2, 3, 1}));
+    }
+}
---
+++ src/main/java/T64.java
@@ -0,0 +1,19 @@
+public class T64 {
+
+    public int minPathSum(int[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        int[] dp = new int[row];
+        dp[0] = grid[0][0];
+        for (int i = 1; i < row; i++) {
+            dp[i] = dp[i - 1] + grid[i][0];
+        }
+        for (int i = 1; i < col; i++) {
+            for (int j = 0; j < row; j++) {
+                dp[j] = (j == 0 ? dp[j] : Math.min(dp[j], dp[j - 1])) + grid[j][i];
+            }
+        }
+        return dp[row - 1];
+    }
+
+}

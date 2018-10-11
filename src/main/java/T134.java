78d7e26150c316ce130da72239b9fc3e4bfa216d
liwangadd <liwangadd@gmail.com>
2018-10-11 09:55:07 +0800

    T134&T54&T56&T57&T59

--- src/main/java/T134.java
+++ src/main/java/T134.java
@@ -1,34 +1,19 @@
-import java.util.Arrays;
-import java.util.Comparator;
-
 public class T134 {
 
-    private class Node {
-        int gas;
-        int cost;
-        int index;
-
-        public Node(int gas, int cost, int index) {
-            this.gas = gas;
-            this.cost = cost;
-            this.index = index;
-        }
-    }
-
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        Node[] stations = new Node[gas.length];
+        int total = 0;
+        int tank = 0;
+        int index = 0;
         for (int i = 0; i < gas.length; ++i) {
-            stations[i] = new Node(gas[i], cost[i], i);
+            tank += gas[i] - cost[i];
+            if (tank < 0) {
+                tank = 0;
+                index = i+1;
+            }
+            total += gas[i] - cost[i];
         }
-
-        Arrays.sort(stations, Comparator.comparingInt(o -> o.gas - o.cost));
-        int currentGas = 0;
-        for (int i = 0; i < stations.length - 1; ++i) {
-            currentGas += stations[i].gas;
-            if (currentGas < stations[i + 1].cost) return -1;
-            currentGas -= stations[i].cost;
-        }
-        return stations[0].index;
+        if (total < 0) return -1;
+        return index;
     }
 
 }

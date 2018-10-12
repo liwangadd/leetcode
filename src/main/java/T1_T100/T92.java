c60f6b6f8f9a1fc44e607e2424561d020d7d49cb
liwangadd <liwangadd@gmail.com>
2018-10-12 23:53:33 +0800

    T89&T92

---
+++ src/main/java/T1_T100/T92.java
@@ -0,0 +1,54 @@
+package T1_T100;
+
+public class T92 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static ListNode reverseBetween(ListNode head, int m, int n) {
+
+        if (m == n || head == null) return head;
+
+        ListNode newHead = new ListNode(-1);
+        newHead.next = head;
+
+        ListNode beforeTail = newHead;
+        int index = 1;
+        while (index < m) {
+            beforeTail = beforeTail.next;
+            ++index;
+        }
+
+        ListNode pre = beforeTail.next, current=null;
+        ListNode middleTail = pre, nextHead=pre.next;
+        for (; index < n; ++index) {
+            current=nextHead;
+            nextHead = nextHead.next;
+            current.next = pre;
+            pre = current;
+        }
+        beforeTail.next = current;
+        middleTail.next = nextHead;
+        return newHead.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next=new ListNode(2);
+        head.next.next=new ListNode(3);
+        head.next.next.next=new ListNode(4);
+        head.next.next.next.next=new ListNode(5);
+        ListNode newHead = reverseBetween(head, 2, 3);
+        while(newHead!=null){
+            System.out.println(newHead.val);
+            newHead=newHead.next;
+        }
+    }
+
+}

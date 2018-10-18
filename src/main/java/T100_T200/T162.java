2d35dec2d8ed7774ffb62c5d7c4c87f3f193d76c
liwangadd <liwangadd@gmail.com>
2018-10-18 19:13:28 +0800

    T132&T135&T146...

---
+++ src/main/java/T100_T200/T162.java
@@ -0,0 +1,16 @@
+package T100_T200;
+
+public class T162 {
+
+    public int findPeakElement(int[] nums) {
+        if (nums.length == 0) return -1;
+        if(nums.length==1) return 0;
+        if (nums[0] > nums[1]) return 0;
+        if (nums[nums.length - 1] > nums[nums.length - 2]) return nums.length - 1;
+        for (int i = 1; i < nums.length - 1; ++i) {
+            if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) return i;
+        }
+        return -1;
+    }
+
+}

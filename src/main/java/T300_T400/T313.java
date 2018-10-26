baa7516e46b8aef04cce8245eebeea833ccaaf19
liwangadd <liwangadd@gmail.com>
2018-10-26 19:14:32 +0800

    T170&T313&T318&T328

---
+++ src/main/java/T300_T400/T313.java
@@ -0,0 +1,26 @@
+package T300_T400;
+
+import com.sun.media.sound.UlawCodec;
+
+import java.util.Arrays;
+
+public class T313 {
+
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        int[] ugly = new int[n];
+        int[] idx = new int[primes.length];
+        int[] vals = new int[primes.length];
+        Arrays.fill(vals, 1);
+        ugly[0] = 1;
+        int next = 1;
+        for (int i = 0; i < n; ++i) {
+            ugly[i] = next;
+            for (int j = 0; j < primes.length; ++j) {
+                if (vals[j] == ugly[i]) vals[j] = ugly[idx[j]++] * primes[j];
+                next = Math.min(next, vals[j]);
+            }
+        }
+        return ugly[n - 1];
+    }
+
+}
---
+++ src/main/java/T300_T400/T318.java
@@ -0,0 +1,23 @@
+package T300_T400;
+
+public class T318 {
+
+    public int maxProduct(String[] words) {
+        int[] values = new int[words.length];
+        for (String word : words) {
+            for (int i = 0, len = word.length(); i < len; ++i) {
+                values[i] |= i << (word.charAt(i) - 'a');
+            }
+        }
+        int max = 0;
+        for (int i = 0; i < words.length; ++i) {
+            for (int j = i + 1; j < words.length; ++j) {
+                if ((values[i] & values[j]) == 0 && words[i].length() * words[j].length() > max) {
+                    max = words[i].length() * words[j].length();
+                }
+            }
+        }
+        return max;
+    }
+
+}
